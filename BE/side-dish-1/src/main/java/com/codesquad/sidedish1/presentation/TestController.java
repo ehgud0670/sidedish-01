f89db93f64a230a3ae1ff93955ea630b10d1457c
Jenny <huji3156@gmail.com>
2020-04-22 01:32:19 +0900

    [BE] feat : make category repository

--- BE/side-dish-1/src/main/java/com/codesquad/sidedish1/presentation/TestController.java
+++ BE/side-dish-1/src/main/java/com/codesquad/sidedish1/presentation/TestController.java
@@ -1,4 +1,34 @@
 package com.codesquad.sidedish1.presentation;
 
+package com.example.demo;
+
+
+import com.example.demo.Entity.Banchan;
+import com.example.demo.Entity.Category;
+import com.example.demo.Entity.Image;
+import org.simpleflatmapper.jdbc.JdbcMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class TestController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/test1")
+    public Category findCategoryById() {
+        return categoryRepository.findCategoryWithBanchans(1L);
+    }
+
+    @GetMapping("/test2")
+    public List<Category> findAllCategoriesById() {
+        return categoryRepository.findAllCategoryWithBanchans();
+    }
+
+
 }
