2ebab6cc3ef825eed5389524b2490902b97f7663
ehgud0670 <kimdo2297@gmail.com>
2020-04-21 16:59:15 +0900

    [iOS] Feat: 로그인 버튼 추가하여 오토레이아웃 적용함

---
+++ iOS/Sidedish/Sidedish/Views/LoginButton.swift
@@ -0,0 +1,25 @@
+//
+//  LoginButton.swift
+//  Sidedish
+//
+//  Created by kimdo2297 on 2020/04/21.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class LoginButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLayer()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureLayer()
+    }
+    
+    private func configureLayer() {
+        layer.cornerRadius = 10
+    }
+}
