63e47f13d947e9e3156fe8d724716e513a41accd
ehgud0670 <kimdo2297@gmail.com>
2020-04-21 12:23:59 +0900

    [iOS] Chore: fix untracked files

---
+++ iOS/Sidedish/Sidedish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sidedish
+//
+//  Created by kimdo2297 on 2020/04/21.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
