0d20703fcae29bd11c4147c8cbcf127e2ccfaedc
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-22 22:16:28 +0900

    [FE] Feat : Button Component 의 JSX, CSS 개발
    Title >
    * Button Component 의 JSX, CSS 개발
    
    Description>
    * Local Navigation 에서 공용으로 사용되는 버튼에 대한 JSX, CSS 개발
    
    associated with #9

--- FE/src/LocalNavigation/PublicComponent/Button.js
+++ FE/src/LocalNavigation/PublicComponent/Button.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import styled, { css } from 'styled-components'
+
+const Div = styled.div`
+  float: left;
+
+  ${props => 
+    props.direction === "right" && 
+    css`
+      float: right;
+  `};
+`;
+
+const Anchor = styled.a`
+  padding: 10px 10px 10px 10px;
+  background: url(http://dev-angelo.dlinkddns.com/util_bar.gif) no-repeat;
+  background-position: left;
+`;
 
 const Button = props => {
   return (
-    ``
+    <Div direction={props.direction}>
+    <Anchor href={props.url}>{props.title}</Anchor>
+    </Div>
   );
 }
 
