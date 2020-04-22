171d719603dd396e3b3d7ebfe151ad873e020d2b
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-22 22:21:25 +0900

    [FE] Feat : Local Navigation Component JSX, CSS 개발
    Title >
    * Local Navigation Component 의 JSX, CSS 개발
    
    Description>
    * 최상단 Navigation 내용을 보여주는 컴포넌트 개발.
    * 공용 컴포넌트인 Button, ListButton, MenuButton 을 이용.
    * MockData 를 통해 요구사항을 만족하는 UI 확인.
    
    associated with #9

--- FE/src/LocalNavigation/LocalNavigation.js
+++ FE/src/LocalNavigation/LocalNavigation.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components'
+
+import Button from './PublicComponent/Button'
+import ListButton from './PublicComponent/ListButton'
+import localNavigationData from '../MockData/LocalNavigationData'
+
+const WrapDiv = styled.div`
+  position: relative;
+`;
+
+const ContentDiv = styled.div`
+  width: 980px;
+  height: 36px;
+  line-height: 36px;
+  margin: 0 auto;
+  position: relative;
+`;
 
 const LocalNavigation = () => {
+  const [appDownloadInfo, setAppDownloadInfo] = useState();
+  const [commonMenuInfo, setCommonMenuInfo] = useState();
+
+  useEffect(() => {
+    //These flow can be changed to get data from server by fetching.
+    requestAppDownloadData()
+      .then(
+        setAppDownloadInfo(localNavigationData.download));
+
+    requestLNBMenuData()
+      .then(setCommonMenuInfo(localNavigationData.common));
+  }, []);
+
+  const requestAppDownloadData = () => {
+    return new Promise(function(resolve, reject) {
+      resolve(localNavigationData.download);
+    });
+  }
+
+  const requestLNBMenuData = () => {
+    return new Promise(function(resolve, reject) {
+      resolve(localNavigationData.menu);
+    });
+  }
+
   return (
-    ``
+    <WrapDiv>
+      <ContentDiv>
+        {appDownloadInfo !== undefined && <ListButton title={appDownloadInfo.title} url={appDownloadInfo.url} list={appDownloadInfo.list}/>}
+        {commonMenuInfo !== undefined && 
+         commonMenuInfo.map((v, i) => (
+           v.list === undefined ?  
+            <Button title={v.title} url={v.url} direction="right" /> : 
+            <ListButton title={v.title} url={v.url} list={v.list} direction="right" />
+            )
+         ).reverse()
+        }
+      </ContentDiv>
+    </WrapDiv>
   );
 }
 
