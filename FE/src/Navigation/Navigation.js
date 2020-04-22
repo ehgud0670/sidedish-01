c1530e6c47b4f2a241710bc8e1940e6524bcc128
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-23 03:13:53 +0900

    [FE] Feat : Navigation Component 의 JSX, CSS 개발
    Title >
    * Navigation Component 의 JSX, CSS 개발
    
    Description>
    * 카테고리들을 보여주는 컴포넌트에 대한 JSX, CSS 개발.
    
    associated with #12

--- FE/src/Navigation/Navigation.js
+++ FE/src/Navigation/Navigation.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components'
+import ListButton from './PublicComponent/ListButton'
+import navigationData from '../MockData/NavigationData'
+
+const NavigationWrapDiv = styled.div`
+  position: relative;
+  background: #483f35;
+`;
+
+const NavigationDiv = styled.nav`
+  width: 980px;
+  height: 52px;
+  margin: 0 auto;
+`;
+
+const NavigationListDiv = styled.div`
+  position: relative;
+`;
 
 const Navigation = () => {
+  const [menuInfo, setMenuInfo] = useState();
+
+  useEffect(() => {
+    //These flow can be changed to get data from server by fetching.
+    requestAppDownloadData()
+      .then(
+        setMenuInfo(navigationData));
+  }, []);
+
+  const requestAppDownloadData = () => {
+    return new Promise(function(resolve, reject) {
+      resolve(navigationData);
+    });
+  }
+
   return (
-    ``
+    <NavigationWrapDiv>
+      <NavigationDiv>
+        <NavigationListDiv>
+        {menuInfo !== undefined && 
+         menuInfo.map((v, i) => (
+           <ListButton key={i} url="#" title={v.title} list={v.list}></ListButton>
+          )
+         )
+        }
+        </NavigationListDiv>
+      </NavigationDiv>
+    </NavigationWrapDiv>
   );
 }
 
