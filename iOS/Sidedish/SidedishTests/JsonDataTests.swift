377a4bd7ccb91035934e9b5b6d5e00731b274a25
ehgud0670 <kimdo2297@gmail.com>
2020-04-23 14:43:58 +0900

    [iOS] Feat: 1. stub json 파일 추가하고 json 데이터를 반환하는 정적 메소드 구현함 	    2. 데이터가 잘 리턴되는지 테스트 코드 작성함

---
+++ iOS/Sidedish/SidedishTests/JsonDataTests.swift
@@ -0,0 +1,27 @@
+//
+//  JsonDataTests.swift
+//  SidedishTests
+//
+//  Created by kimdo2297 on 2020/04/23.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import XCTest
+@testable import Sidedish
+
+class JsonDataTests: XCTestCase {
+    func testSuccessCategoryURLsResponseStub() {
+        let data = Data.jsonData(forResource: "SuccessCategoryURLsResponseStub")
+        XCTAssertNotNil(data)
+    }
+    
+    func testSuccessCategoryResponseStub() {
+        let data = Data.jsonData(forResource: "SuccessCategoryResponseStub")
+        XCTAssertNotNil(data)
+    }
+    
+    func testSuccessProductDetailResponseStub() {
+        let data = Data.jsonData(forResource: "SuccessProductDetailResponseStub")
+        XCTAssertNotNil(data)
+    }
+}
