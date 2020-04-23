f0dbbc7c426e13c743e2b3f3e8124b091fbf3b93
ehgud0670 <kimdo2297@gmail.com>
2020-04-23 15:50:45 +0900

    [iOS] Feat: categoryURLs를 request하여 받은 응답을 처리하는 모델 객체 생성 및 	    잘 처리되었는지 테스트 코드 작성함 	    => 잘 처리됨
    associate with #7

---
+++ iOS/Sidedish/SidedishTests/CodableTests.swift
@@ -0,0 +1,33 @@
+//
+//  CodableTests.swift
+//  SidedishTests
+//
+//  Created by kimdo2297 on 2020/04/23.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import XCTest
+@testable import Sidedish
+
+final class CodableTests: XCTestCase {
+    func testURLsResponse() {
+        let mock = MockCategoryURLsSuccessStub()
+        do {
+            try mock.requestResource(from: "",
+                                     httpMethod: .get, httpBody: nil,
+                                     completionHandler: { data, _, _ in
+                                        guard let data = data else { return }
+                                        let response = try? JSONDecoder().decode(CategoryURLsResponse.self,
+                                                                                       from: data)
+                                        XCTAssertNotNil(response)
+            })
+        } catch {
+            switch  error {
+            case NetworkErrorCase.invalidURL:
+                print("invalidURL")
+            default:
+                break
+            }
+        }
+    }
+}
