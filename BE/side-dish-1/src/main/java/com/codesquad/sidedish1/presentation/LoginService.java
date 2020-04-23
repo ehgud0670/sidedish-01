9ae30f7439cc4bd5fa07bafd16d4634c441b4c86
Hamill210 <han95210@naver.com>
2020-04-24 00:43:57 +0900

    [BE] feat : LoginService 구현
    - Access Token을 발급받기 위해 필요한 정보를 Map에 넣음
    - HttpEntity와 RestTemplate을 이용하여 모양새를 만들고 AccessTokenResponseSuccess 클래스에 넣어주는 형태로 return값을 만듦
    
    associate with #17

--- BE/side-dish-1/src/main/java/com/codesquad/sidedish1/presentation/LoginService.java
+++ BE/side-dish-1/src/main/java/com/codesquad/sidedish1/presentation/LoginService.java
@@ -3,23 +3,44 @@ package com.codesquad.sidedish1.presentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LoginService {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    private String URL = "https://github.com/login/oauth/access_token";
+    private final String URL = "https://github.com/login/oauth/access_token";
     @Value("${github.client_id}")
     private String clientId;
-    @Value("${github.client_secret")
+    @Value("${github.client_secret}")
     private String clientSecret;
 
-//    public AccessTokenResponseSuccess getAccessToken(@PathParam("code") String code, HttpServletResponse response) {
-//
-//    }
+    public AccessTokenResponseSuccess getAccessToken(String code) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        Map<String, String> header = new HashMap<>();
+        header.put("Accept", "application/json");
+        headers.setAll(header);
+
+        MultiValueMap<String, String> bodies = new LinkedMultiValueMap<>();
+        Map<String, String> body = new HashMap<>();
+        body.put("client_id", clientId);
+        body.put("client_secret", clientSecret);
+        body.put("code", code);
+        bodies.setAll(body);
+
+        HttpEntity<?> request = new HttpEntity<>(bodies,headers);
+        ResponseEntity<?> response = new RestTemplate().postForEntity(URL, request, AccessTokenResponseSuccess.class);
+        log.info("##### response: {}", response);
+        log.info("##### response: {}", response.getBody());
+        return (AccessTokenResponseSuccess) response.getBody();
+    }
 
 }
