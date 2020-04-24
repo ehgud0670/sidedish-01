27e828edb05ea24226ed2dbad2fd90d52bbfd208
Hamill210 <han95210@naver.com>
2020-04-24 21:19:36 +0900

    [BE] feat : 저장된 유저 객체를 json 형태로 전달
    associate with #17

--- BE/side-dish-1/src/main/java/com/codesquad/sidedish1/presentation/LoginController.java
+++ BE/side-dish-1/src/main/java/com/codesquad/sidedish1/presentation/LoginController.java
@@ -1,13 +1,9 @@
 package com.codesquad.sidedish1.presentation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -19,7 +15,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 import java.io.IOException;
-import java.util.*;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
 public class LoginController {
@@ -49,7 +46,7 @@ public class LoginController {
     }
 
     @GetMapping("/GetGithubUserInfo")
-    public String callAPI(HttpServletResponse response) throws NullPointerException{
+    public String callAPI(HttpServletResponse response) throws NullPointerException {
         Token token = tokenRepository.findById(1L).orElseThrow(NoSuchElementException::new);
         String jsonInString = "";
 
@@ -87,8 +84,9 @@ public class LoginController {
     }
 
     @GetMapping("/final")
-    public User userData() {
-        return userRepository.findById(1L).orElseThrow(NoSuchElementException::new);
+    public ResponseEntity<User> userData(HttpServletResponse response) throws IOException {
+        User user = userRepository.findById(1L).orElseThrow(NoSuchElementException::new);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
 }
