ff7b4564dcb50417b36c1e6676d272a856d183e6
Hamill210 <han95210@naver.com>
2020-04-25 14:29:17 +0900

    [BE] feat : 에러 처리 추가
    - 간단한 에러 처리를 추가하고 에러 발생 시 적절한 에러 메세지 전달
    
    associate with #17

---
+++ BE/side-dish-1/src/main/java/com/codesquad/sidedish1/exception/ErrorMessages.java
@@ -0,0 +1,8 @@
+package com.codesquad.sidedish1.exception;
+
+public class ErrorMessages {
+
+    public static final String ERROR = "ERROR";
+    public static final String METHOD_ARGUMENT_NOT_VALID_EXCEPTION = " 값이 유효하지 않거나 조건이 부합하지 않습니다";
+    public static final String HTPP_REQUEST_METHOD_NOT_SUPPORTED_EXCEPTION = "지원하지 않은 HTTP Method를 호출했습니다";
+}
