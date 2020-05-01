eb71fb93d229290b7180c6191e56ea46497b3346
ehgud0670 <kimdo2297@gmail.com>
2020-05-01 19:38:33 +0900

    [iOS] Refactor: 기존의 totalView가 필요없어져서 없앰       Feat: 수평 스크롤뷰를 추가해 맨 위에 자리 잡게 하고 안에 스택뷰를 추가해서 뷰를 추가하게 함 	    또 페이징 모드로 넘어가도록 기능 구현함

--- iOS/Sidedish/Sidedish/ViewControllers/DetailViewController.swift
+++ iOS/Sidedish/Sidedish/ViewControllers/DetailViewController.swift
@@ -43,39 +43,75 @@ final class DetailViewController: UIViewController {
 }
 
 final class DetailVerticalScrollView: UIScrollView {
-    private let detailTotalView = DetailTotalView()
+    private let thumbScrollView = ThumbScrollView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        configureDetailTotalView()
+        configureThumbScrollView()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         translatesAutoresizingMaskIntoConstraints = false
-        configureDetailTotalView()
     }
     
-    private func configureDetailTotalView() {
-        addSubview(detailTotalView)
+    private func configureThumbScrollView() {
+        addSubview(thumbScrollView)
         
-        detailTotalView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-        detailTotalView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        detailTotalView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        contentLayoutGuide.heightAnchor.constraint(equalTo: detailTotalView.heightAnchor).isActive = true
+        thumbScrollView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+        thumbScrollView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        thumbScrollView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        contentLayoutGuide.topAnchor.constraint(equalTo: thumbScrollView.topAnchor).isActive = true
     }
 }
 
-final class DetailTotalView: UIView {
+final class ThumbScrollView: UIScrollView {
+    private let thumbStackView = ThumbStackView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
+        isPagingEnabled = true
+        configureThumbStackView()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         translatesAutoresizingMaskIntoConstraints = false
+        isPagingEnabled = true
+        configureThumbStackView()
+    }
+    
+    private func configureThumbStackView() {
+        addSubview(thumbStackView)
+        
+        thumbStackView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        thumbStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        thumbStackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        contentLayoutGuide.widthAnchor.constraint(equalTo: thumbStackView.widthAnchor).isActive = true
+    }
+    
+    func addToStack(view: UIView) {
+        thumbStackView.addArrangedSubview(view)
+        view.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
     }
 }
 
+final class ThumbStackView: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        axis = .horizontal
+        distribution = .fill
+    }
+}
